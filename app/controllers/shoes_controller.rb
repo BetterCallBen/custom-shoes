cd1966aaad7a0949fd8e8f982ad368c32660de35
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-30 00:47:55 +0200

    crud

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,49 @@
 class ShoesController < ApplicationController
+  def index
+    @shoes = Shoe.all
+
+  end
+
+  def show
+    @shoe = Shoe.find(params[:id])
+  end
+
+  def new
+    @shoe = Shoe.new
+  end
+
+  def create
+    @shoe = Shoe.new(shoe_params)
+    @shoe.user = current_user
+    if @shoe.save
+      redirect_to shoes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @shoe = Shoe.find(params[:id])
+  end
+
+  def update
+    @shoe = Shoe.find(params[:id])
+    if @shoe.update(shoe_params)
+      redirect_to shoes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @shoe = Shoe.find(params[:id])
+    @shoe.destroy
+    redirect_to shoes_path
+  end
+
+  private
+
+  def shoe_params
+    params.require(:shoe).permit(:name, :description, :image)
+  end
 end
