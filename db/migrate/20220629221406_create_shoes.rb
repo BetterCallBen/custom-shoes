c07d21601d8658fda4d99b63cd9205b068f34014
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-30 00:48:05 +0200

    add some fields

--- db/migrate/20220629221406_create_shoes.rb
+++ db/migrate/20220629221406_create_shoes.rb
@@ -2,6 +2,10 @@ class CreateShoes < ActiveRecord::Migration[6.0]
   def change
     create_table :shoes do |t|
       t.string :amount
+      t.string :name
+      t.string :description
+      t.string :photos
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
