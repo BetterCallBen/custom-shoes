cf8b881bd747db0a271ec668e14d729db54d4dd3
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-30 01:41:08 +0200

    change filds

--- db/migrate/20220629221406_create_shoes.rb
+++ db/migrate/20220629221406_create_shoes.rb
@@ -1,10 +1,9 @@
 class CreateShoes < ActiveRecord::Migration[6.0]
   def change
     create_table :shoes do |t|
-      t.string :amount
+      t.integer :amount
       t.string :name
       t.string :description
-      t.string :photos
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
